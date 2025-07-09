b9688c50f949c5f29be0c3e9463c992c1b37a8e8
calcudaus <dausg878@gmail.com>
2025-07-09 22:51:05 +0700

    #update: implement post detail view and update post listing with slugs

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,11 +2,14 @@
 
     @foreach ($posts as $post)
         <article class="py-8 max-w-screen-md border-b border-gray-300">
-            <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900"> {{ $post['title'] }}</h2>
+            <a href="posts/{{ $post['slug'] }}" class="hover:underline">
+                <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900"> {{ $post['title'] }}</h2>
+            </a>
             <div class="text-gray-500">
                 <a href="#">{{ $post['author'] }}</a> | 1 January 2028
                 <p class="my-4 font-light">{{ Str::limit($post['body'], 100) }}</p>
-                <a href="#" class="text-blue-500 font-medium hover:underline">Read More &raquo;</a>
+                <a href="posts/{{ $post['slug'] }}" class="text-blue-500 font-medium hover:underline">Read More
+                    &raquo;</a>
             </div>
         </article>
     @endforeach
--- routes/web.php
+++ routes/web.php
@@ -10,12 +10,61 @@ Route::get('/', function () {
 
 Route::get('/posts', function () {
     $posts = [
-        ['title' => 'First Post', 'author' => 'Mhd Firdaus', 'body' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'],
-        ['title' => 'Second Post', 'author' => 'Nisa Madina', 'body' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'],
-        ['title' => 'Third Post', 'author' => 'Aulia Rahman', 'body' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'],
+        [
+            'id' => 1,
+            'slug' => 'first-post',
+            'title' => 'First Post',
+            'author' => 'Mhd Firdaus',
+            'body' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+        ],
+        [
+            'id' => 2,
+            'slug' => 'second-post',
+            'title' => 'Second Post',
+            'author' => 'Nisa Madina',
+            'body' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+        ],
+        [
+            'id' => 3,
+            'slug' => 'third-post',
+            'title' => 'Third Post',
+            'author' => 'Aulia Rahman',
+            'body' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+        ],
     ];
     return view('posts', ['title' => 'Blog', 'posts' => $posts]);
 });
+
+Route::get('/posts/{slug}', function ($slug) {
+    $posts = [
+        [
+            'id' => 1,
+            'slug' => 'first-post',
+            'title' => 'First Post',
+            'author' => 'Mhd Firdaus',
+            'body' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+        ],
+        [
+            'id' => 2,
+            'slug' => 'second-post',
+            'title' => 'Second Post',
+            'author' => 'Nisa Madina',
+            'body' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+        ],
+        [
+            'id' => 3,
+            'slug' => 'third-post',
+            'title' => 'Third Post',
+            'author' => 'Aulia Rahman',
+            'body' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+        ],
+    ];
+    $post = Arr::first($posts, function ($post) use ($slug) {
+        return $post['slug'] === $slug;
+    });
+    return view('post', ['title' => $post['title'], 'post' => $post]);
+});
+
 Route::get('/about', function () {
     return view('about', ['title' => 'About']);
 });
