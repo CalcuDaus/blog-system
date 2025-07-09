f95c5bff3e030330a11806e62855338929199ea3
calcudaus <dausg878@gmail.com>
2025-07-09 22:11:56 +0700

    #update: refactor blog routes and views to use /posts, add posts listing page

--- resources/views/blog.blade.php
+++
@@ -1,4 +0,0 @@
-<x-layout :title="$title">
-    <p>Blog page content goes here.</p>
-    <p>This is a simple Blog page to demonstrate the layout and components.</p>
-</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,14 @@ Route::get('/', function () {
 });
 
 
-Route::get('/blog', function () {
-    return view('blog', ['title' => 'Blog']);
+
+Route::get('/posts', function () {
+    $posts = [
+        ['title' => 'First Post', 'author' => 'Mhd Firdaus', 'body' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'],
+        ['title' => 'Second Post', 'author' => 'Nisa Madina', 'body' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'],
+        ['title' => 'Third Post', 'author' => 'Aulia Rahman', 'body' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'],
+    ];
+    return view('posts', ['title' => 'Blog', 'posts' => $posts]);
 });
 Route::get('/about', function () {
     return view('about', ['title' => 'About']);
