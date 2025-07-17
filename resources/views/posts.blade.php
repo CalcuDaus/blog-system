773c9a077f790a2db4c9537d12046ab555479d8d
calcudaus <dausg878@gmail.com>
2025-07-17 08:55:39 +0700

    #update: refactor Post model to include author relationship, update PostFactory to use author_id, and modify views and routes for author display

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -6,7 +6,7 @@
                 <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900"> {{ $post['title'] }}</h2>
             </a>
             <div class="text-gray-500">
-                <a href="#">{{ $post['author'] }}</a> | 1 January 2028
+                <a href="authors/{{ $post->author->id }}">{{ $post->author->name }}</a> | 1 January 2028
                 <p class="my-4 font-light">{{ Str::limit($post['body'], 100) }}</p>
                 <a href="posts/{{ $post['slug'] }}" class="text-blue-500 font-medium hover:underline">Read More
                     &raquo;</a>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -8,10 +9,12 @@ Route::get('/', function () {
 });
 
 
-
 Route::get('/posts', function () {
     $posts = Post::all();
     return view('posts', ['title' => 'Blog', 'posts' => $posts]);
+});
+Route::get('/authors/{user}', function (User $user) {
+    return view('posts', ['title' => 'Article By ' . $user->name, 'posts' => $user->posts]);
 });
 
 Route::get('/posts/{post:slug}', function (Post $post) {
